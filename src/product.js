e6a01a0a883d674ab40cd2342518625fa9c9279f
ThiernoAmadouBa <bathierno1910@gmail.com>
2025-02-12 16:53:26 +0000

    first commit

---
+++ src/product.js
@@ -0,0 +1,9 @@
+const product = {
+    name: "Smartphone Pro X12",
+    price: "$999",
+    description: "A high-end smartphone with a sleek design, powerful performance, and stunning display.",
+    image: "https://via.placeholder.com/300" // Image de remplacement
+  };
+  
+  export default product;
+  
